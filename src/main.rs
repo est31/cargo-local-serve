0835a622563385adca3f78c787e69f95766683d9
est31 <MTest31@outlook.com>
2017-11-12 21:19:45 +0100

    Stronger CSP policy
    Default to deny, and only allow stuff we need.
    Sadly we still need to allow inline CSS but
    maybe one day we can avoid that!

--- src/main.rs
+++ src/main.rs
@@ -103,19 +103,28 @@ header! { (ContentSecurityPolicy, "Content-Security-Policy") => [String] }
 
 fn csp_hdr(req :&mut Request, mut res :Response) -> IronResult<Response> {
 	let mut csp_header =
-		"default-src 'self'; \
-		object-src 'none'; \
-		connect-src 'none'; \
-		script-src 'none'".to_owned();
+		"default-src 'none'; \
+		img-src 'self'; \
+		form-action 'self'; ".to_owned();
+
 	let path = req.url.path();
-	if let Some(z) = path.get(0) {
+	let allow_inline_style = if let Some(z) = path.get(0) {
 		// TODO find a way to avoid inline css in the syntect formatter
 		// and then remove || z == &"crate".
 		// https://github.com/trishume/syntect/issues/121
 		if z == &"static" || z == &"crate" {
 			// Needed for inline CSS inside SVG
-			csp_header += "; style-src 'self' 'unsafe-inline'";
+			true
+		} else {
+			false
 		}
+	} else {
+		false
+	};
+	if allow_inline_style {
+		csp_header += "style-src 'self' 'unsafe-inline';";
+	} else {
+		csp_header += "style-src 'self';";
 	}
 	res.headers.set(ContentSecurityPolicy(csp_header));
 	Ok(res)
