0cb98a0c2dbbe10df8bb52fc42e00a3e7c534a0d
est31 <MTest31@outlook.com>
2017-12-06 12:15:27 +0100

    Add two commands useful for research purposes
    analyze-all-crates currently runs some statistics
    about storage space on the crates.
    
    diff-all-crates attempts to reconstruct crate files
    from their content only. Right now it only
    reconstructs at the gzip layer. For this purpose,
    we fixate the version of flate2 so thoroughly.

---
+++ src/bin/analyze-all-crates.rs
@@ -0,0 +1,255 @@
+extern crate cargo_local_serve;
+extern crate flate2;
+extern crate ring;
+extern crate tar;
+extern crate pbr;
+
+use std::fs::File;
+use std::io;
+use std::env;
+use std::path::Path;
+use cargo_local_serve::registry::registry;
+use self::registry::{Registry, AllCratesJson};
+use ring::digest::{Context, SHA256};
+use flate2::{Compression, GzBuilder};
+use flate2::read::GzDecoder;
+use tar::Archive;
+
+use std::thread;
+use std::sync::mpsc::{sync_channel, SyncSender};
+use std::collections::HashMap;
+
+use pbr::MultiBar;
+
+struct HashCtx(Context);
+
+impl io::Write for HashCtx {
+	fn write(&mut self, data: &[u8]) -> Result<usize, io::Error> {
+		self.0.update(data);
+		Ok(data.len())
+	}
+	fn flush(&mut self) -> Result<(), io::Error> {
+		Ok(())
+	}
+}
+
+impl HashCtx {
+	fn finish_and_get_digest_hex(self) -> String {
+		let digest = self.0.finish();
+		let mut hash_str = String::with_capacity(60);
+		for d in digest.as_ref().iter() {
+			hash_str += &format!("{:02x}", d);
+		}
+		hash_str
+	}
+}
+
+// corrupt deflate stream error blacklist
+// TODO: does having it make any sense?
+// https://github.com/rust-lang/cargo/issues/1465
+const BLACKLIST :&[&str] = &[
+	/*
+	"cobs", // v0.1.0
+	"curl-sys", // v0.1.0
+	"expat-sys", // v2.1.0
+	"flate2", // v0.2.2
+	"libbreakpad-client-sys", // v0.1.0
+	"ppapi", // v0.0.1
+	"ruplicity", // v0.1.0
+	"rustc-serialize", // v0.3.8
+	*/
+];
+
+#[derive(Clone)]
+struct Message {
+	t :usize,
+	ctr :usize,
+	total_file_count :usize,
+	msg :Msg,
+}
+#[derive(Clone)]
+enum Msg {
+	Text(String),
+	File(String, String, u64, u64),
+	Done,
+}
+
+fn run(tx :SyncSender<Message>, acj :&AllCratesJson,
+		total_file_count :usize, t :usize, tc :usize,
+		storage_base :&Path) {
+	let mut ctr = 0;
+
+	macro_rules! msg {
+		($msg:expr) => {
+			tx.send(Message {
+				t, ctr, total_file_count,
+				msg : $msg,
+			}).unwrap();
+		}
+	}
+	macro_rules! pln {
+		($($v:expr),*) => {
+			msg!(Msg::Text(format!($($v),*)));
+		}
+	}
+
+	for &(ref name, ref versions) in acj.iter() {
+		let name_path = storage_base.join(registry::obtain_crate_name_path(name));
+		//fs::create_dir_all(&name_c_path).unwrap();
+
+		if BLACKLIST.contains(&&name[..]) {
+			ctr += versions.len();
+			continue;
+		}
+
+		for ref v in versions.iter() {
+			ctr += 1;
+			/*if ctr != 21899 {
+				continue;
+			}*/
+			if ctr % tc != t {
+				continue;
+			}
+			let crate_file_path = name_path
+				.join(format!("{}-{}.crate", name, v.version));
+			match File::open(&crate_file_path) {
+				Ok(mut f) => {
+					// verify the checksum
+					let mut ring_ctx = HashCtx(Context::new(&SHA256));
+					io::copy(&mut f, &mut ring_ctx).unwrap();
+					let hash_str = ring_ctx.finish_and_get_digest_hex();
+					if hash_str == v.checksum {
+						// everything is fine!
+					} else {
+						pln!("Checksum mismatch for {} v{}. \
+								Deleting. expected: '{}' was: '{}'",
+								name, v.version, v.checksum, hash_str);
+						// Ignore
+						continue;
+					}
+				},
+				Err(_) => {
+					pln!("File not found for {} v{}", name, v.version);
+					// Ignore
+					continue;
+				},
+			}
+			pln!("Diffing {} v{}", name, v.version);
+
+			// Do the diffing.
+			let f = File::open(&crate_file_path).unwrap();
+			let gz_dec = GzDecoder::new(f);
+			//pln!("{:?}", gz_dec.header());
+			let mut archive = Archive::new(gz_dec);
+			//gz_dec.
+
+			for entry in archive.entries().expect("no") {
+				let mut entry = match entry {
+					Ok(e) => e,
+					Err(e) => {
+						pln!("ERROR FOR {} v{}: {:?}", name, v.version, e);
+						break;
+					},
+				};
+				let mut buffer = Vec::<u8>::new();
+				match io::copy(&mut entry, &mut buffer) {
+					Ok(_) => (),
+					Err(e) => pln!("ERROR FOR {} v{}: {:?}", name, v.version, e),
+				}
+				let mut ring_ctx = HashCtx(Context::new(&SHA256));
+				let mut sl :&[u8] = &buffer;
+				io::copy(&mut sl, &mut ring_ctx).unwrap();
+				let hash_str = ring_ctx.finish_and_get_digest_hex();
+				let mut sl :&[u8] = &buffer;
+				let mut gz_enc = GzBuilder::new().read(sl, Compression::best());
+				let mut buffer_compressed = Vec::<u8>::new();
+				io::copy(&mut gz_enc, &mut buffer_compressed).unwrap();
+				let compressed_size = buffer_compressed.len() as u64;
+				let path_str = entry.path().unwrap().to_str().unwrap().to_string();
+				let size = entry.header().entry_size().unwrap();
+				msg!(Msg::File(hash_str, path_str, size, compressed_size));
+			}
+		}
+		/*if ctr > 10_000 {
+			pln!("abort");
+			break;
+		}*/
+	}
+	msg!(Msg::Done);
+}
+
+fn main() {
+	println!("Loading all crates json...");
+	let registry = Registry::from_name("github.com-1ecc6299db9ec823").unwrap();
+	let acj :AllCratesJson = registry.get_all_crates_json().unwrap();
+	let total_file_count :usize = acj.iter().map(|&(_, ref v)| v.len()).sum();
+
+	println!("The target is {} files.", total_file_count);
+	let storage_base = env::current_dir().unwrap().join("crate-archives");
+	println!("Using directory {} to load the files from.",
+		storage_base.to_str().unwrap());
+
+	let (tx, rx) = sync_channel(10);
+
+	let thread_count = 8;
+	for v in 0..thread_count {
+		let tx = tx.clone();
+		let acj = acj.clone();
+		let storage_base = storage_base.clone();
+		thread::spawn(move || {
+			run(tx, &acj, total_file_count, v, thread_count,
+				&storage_base);
+		});
+	}
+	let mut h_map = HashMap::new();
+	let mut file_count = 0;
+	let mut threads_running = thread_count;
+
+	let mut mb = MultiBar::new();
+	mb.println("Analyzing all crates");
+	let mut p_list = (0..thread_count)
+		.map(|_| mb.create_bar((total_file_count/thread_count) as _))
+		.collect::<Vec<_>>();
+
+	thread::spawn(move || {
+		mb.listen();
+		println!("all bars done!");
+	});
+
+	while let Ok(msg) = rx.recv() {
+		match msg.msg {
+			Msg::Text(_s) => {
+				//println!("[{}/{}] {}", msg.ctr, msg.total_file_count, s),
+				p_list[msg.t].inc();
+			}
+			Msg::File(h, path, size, size_c) => {
+				file_count += 1;
+				let l = h_map.entry(h).or_insert((Vec::new(), size, size_c));
+				l.0.push(path);
+			},
+			Msg::Done => {
+				//println!("Thread {} done", msg.t);
+				p_list[msg.t].finish();
+				threads_running -= 1;
+				if threads_running == 0 {
+					break;
+				}
+			},
+		}
+	}
+	let mut hashes = h_map.into_iter().collect::<Vec<_>>();
+	hashes.sort_by_key((|&(_, ref l)| l.0.len() as u64 * l.1));
+	hashes.reverse();
+
+	println!("File count: {}", file_count);
+	println!("Number of unique file hashes: {}", hashes.len());
+	println!("Total size uncompressed: {}", hashes.iter()
+		.map(|&(_, ref l)| l.0.len() as u64 * l.1).sum::<u64>());
+	println!("Uncompressed size of unique files: {}", hashes.iter()
+		.map(|&(_, ref l)| l.1).sum::<u64>());
+	println!("Compressed size of unique files: {}", hashes.iter()
+		.map(|&(_, ref l)| l.2).sum::<u64>());
+	println!("top used hashes: {:?}",
+		hashes[0..40].iter().map(|&(_, ref l)| (l.0[0].clone(), l.0.len(),
+			l.1, l.0.len() as u64 * l.1)).collect::<Vec<_>>());
+}
---
+++ src/bin/diff-all-crates.rs
@@ -0,0 +1,190 @@
+extern crate cargo_local_serve;
+extern crate flate2;
+extern crate ring;
+
+use std::fs::{self, File};
+use std::io;
+use std::env;
+use std::path::Path;
+use cargo_local_serve::registry::registry;
+use self::registry::{Registry, AllCratesJson};
+use ring::digest::{Context, SHA256};
+use flate2::{Compression, GzBuilder};
+use flate2::read::GzDecoder;
+
+use std::thread;
+use std::sync::mpsc::{sync_channel, SyncSender};
+
+struct HashCtx(Context);
+
+impl io::Write for HashCtx {
+	fn write(&mut self, data: &[u8]) -> Result<usize, io::Error> {
+		self.0.update(data);
+		Ok(data.len())
+	}
+	fn flush(&mut self) -> Result<(), io::Error> {
+		Ok(())
+	}
+}
+
+impl HashCtx {
+	fn finish_and_get_digest_hex(self) -> String {
+		let digest = self.0.finish();
+		let mut hash_str = String::with_capacity(60);
+		for d in digest.as_ref().iter() {
+			hash_str += &format!("{:02x}", d);
+		}
+		hash_str
+	}
+}
+
+// corrupt deflate stream error blacklist
+// TODO: does having it make any sense?
+// https://github.com/rust-lang/cargo/issues/1465
+const BLACKLIST :&[&str] = &[
+	/*
+	"cobs", // v0.1.0
+	"curl-sys", // v0.1.0
+	"expat-sys", // v2.1.0
+	"flate2", // v0.2.2
+	"libbreakpad-client-sys", // v0.1.0
+	"ppapi", // v0.0.1
+	"ruplicity", // v0.1.0
+	"rustc-serialize", // v0.3.8
+	*/
+];
+
+/*
+
+[21898/71082] DIFF FAIL for forust v0.1.0!
+Diffoscope output:
+-00000780: 7080 03fc 42f8 0fbd 883a 3300 2400 00    p...B....:3.$..
++00000780: 7080 03fc 42f8 0fbd 883a 3300 2400 000a  p...B....:3.$...
+
+[29482/71082] DIFF FAIL for helianto v0.1.0-beta1!
+│ -gzip compressed data, was "helianto-0.1.0-beta1.crate", max compression, from Unix
+│ +gzip compressed data, was "helianto-0.1.0-beta1.crate", last modified: Wed Jan 13 23:45:28 2016, max compression, from Unix
+*/
+fn run(tx :SyncSender<(usize, usize, String)>, acj :&AllCratesJson,
+		total_file_count :usize, t :usize, tc :usize,
+		storage_base :&Path, storage_con_base :&Path) {
+	let mut ctr = 0;
+
+	macro_rules! pln {
+		($($v:expr),*) => {
+			tx.send((ctr, total_file_count, format!($($v),*))).unwrap();
+		}
+	}
+
+	for &(ref name, ref versions) in acj.iter() {
+		let name_path = storage_base.join(registry::obtain_crate_name_path(name));
+		let name_c_path = storage_con_base.join(registry::obtain_crate_name_path(name));
+		//fs::create_dir_all(&name_c_path).unwrap();
+
+		if BLACKLIST.contains(&&name[..]) {
+			ctr += versions.len();
+			continue;
+		}
+
+		for ref v in versions.iter() {
+			ctr += 1;
+			/*if ctr != 21899 {
+				continue;
+			}*/
+			if ctr % tc != t {
+				continue;
+			}
+			let crate_file_path = name_path
+				.join(format!("{}-{}.crate", name, v.version));
+			let crate_file_c_path = name_c_path
+				.join(format!("{}-{}.crate", name, v.version));
+			match File::open(&crate_file_path) {
+				Ok(mut f) => {
+					// verify the checksum
+					let mut ring_ctx = HashCtx(Context::new(&SHA256));
+					io::copy(&mut f, &mut ring_ctx).unwrap();
+					let hash_str = ring_ctx.finish_and_get_digest_hex();
+					if hash_str == v.checksum {
+						// everything is fine!
+					} else {
+						pln!("Checksum mismatch for {} v{}. \
+								Deleting. expected: '{}' was: '{}'",
+								name, v.version, v.checksum, hash_str);
+						// Ignore
+						continue;
+					}
+				},
+				Err(_) => {
+					pln!("File not found for {} v{}", name, v.version);
+					// Ignore
+					continue;
+				},
+			}
+			pln!("Diffing {} v{}", name, v.version);
+
+			// Do the diffing.
+			let f = File::open(&crate_file_path).unwrap();
+			let gz_dec = GzDecoder::new(f);
+			let file_name = gz_dec.header().unwrap()
+				.filename().map(|v| v.to_vec());
+			let os = gz_dec.header().unwrap().operating_system();
+			//pln!("{:?}", gz_dec.header());
+			let gz_bld = GzBuilder::new()
+				.operating_system(os);
+			let gz_bld = if let Some(filen) = file_name {
+				gz_bld.filename(filen)
+			} else {
+				gz_bld
+			};
+			let mut gz_enc = gz_bld.read(gz_dec, Compression::best());
+
+			/*let mut f = File::create(&crate_file_c_path).unwrap();
+			io::copy(&mut gz_enc, &mut f).unwrap();*/
+
+			let mut ring_ctx = HashCtx(Context::new(&SHA256));
+			match io::copy(&mut gz_enc, &mut ring_ctx) {
+				Ok(_) => (),
+				Err(e) => pln!("ERROR FOR {} v{}: {:?}", name, v.version, e),
+			}
+			let hash_str = ring_ctx.finish_and_get_digest_hex();
+			if hash_str != v.checksum {
+				pln!("DIFF FAIL for {} v{}!", name, v.version);
+			}
+		}
+		/*if ctr > 100 {
+			pln!("abort");
+			break;
+		}*/
+	}
+	println!("thread {} done", t);
+}
+
+fn main() {
+	println!("Loading all crates json...");
+	let registry = Registry::from_name("github.com-1ecc6299db9ec823").unwrap();
+	let acj :AllCratesJson = registry.get_all_crates_json().unwrap();
+	let total_file_count :usize = acj.iter().map(|&(_, ref v)| v.len()).sum();
+
+	println!("The target is {} files.", total_file_count);
+	let storage_base = env::current_dir().unwrap().join("crate-archives");
+	let storage_con_base = env::current_dir().unwrap().join("crate-constr-archives");
+	println!("Using directory {} to load the files from.",
+		storage_base.to_str().unwrap());
+
+	let (tx, rx) = sync_channel(10);
+
+	let thread_count = 8;
+	for v in 0..thread_count {
+		let tx = tx.clone();
+		let acj = acj.clone();
+		let storage_base = storage_base.clone();
+		let storage_con_base = storage_con_base.clone();
+		thread::spawn(move || {
+			run(tx, &acj, total_file_count, v, thread_count,
+				&storage_base, &storage_con_base);
+		});
+	}
+	while let Ok((ctr, tc, s)) = rx.recv() {
+		println!("[{}/{}] {}", ctr, tc, s);
+	}
+}
