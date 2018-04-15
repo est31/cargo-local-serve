60caacaca1a67af7da2fa8acaf12add4c2cea47d
est31 <MTest31@outlook.com>
2018-04-15 07:11:30 +0200

    Add tool to grep over all crates
    upon request by @retep998

---
+++ all-crate-grep/main.rs
@@ -0,0 +1,96 @@
+extern crate all_crates_storage;
+extern crate grep;
+
+use std::env;
+use std::path::Path;
+use all_crates_storage::registry::registry;
+use all_crates_storage::crate_storage::{CrateSource, FileTreeStorage};
+use self::registry::{Registry, AllCratesJson};
+
+use std::thread;
+use std::sync::mpsc::{sync_channel, SyncSender};
+
+use grep::{GrepBuilder, Grep, Match};
+
+fn run(tx :SyncSender<(usize, usize, String)>, acj :&AllCratesJson,
+		total_file_count :usize, t :usize, tc :usize,
+		grepper :&Grep, storage_base :&Path) {
+	let mut ctr = 0;
+
+	macro_rules! pln {
+		($($v:expr),*) => {
+			tx.send((ctr, total_file_count, format!($($v),*))).unwrap();
+		}
+	}
+
+	let mut crate_source = FileTreeStorage::new(storage_base);
+
+	for &(ref name, ref versions) in acj.iter() {
+
+		for ref v in versions.iter() {
+			ctr += 1;
+			/*if ctr != 21899 {
+				continue;
+			}*/
+			if ctr % tc != t {
+				continue;
+			}
+
+			let mut fh = match crate_source.get_crate_handle_nv(name.to_owned(), v.version.clone()) {
+				Some(f) => f,
+				None => {
+					pln!("Version {} of crate {} not mirrored", v.version, name);
+					continue
+				},
+			};
+
+			let file_list = fh.get_file_list();
+			let mut match_found = false;
+			for file_path in file_list.iter() {
+				let f = fh.get_file(file_path).expect("Path not found in crate file");
+				let mut m = Match::new();
+				if grepper.read_match(&mut m, &f, 0) {
+					pln!("Match found in {} v {} file {}", name, v.version, file_path);
+					match_found = true;
+				}
+			}
+			if !match_found {
+				pln!("No match in {} v {}", name, v.version);
+			}
+		}
+	}
+	println!("thread {} done", t);
+}
+
+fn main() {
+	println!("Loading all crates json...");
+	let registry = Registry::from_name("github.com-1ecc6299db9ec823").unwrap();
+	let acj :AllCratesJson = registry.get_all_crates_json().unwrap();
+	let total_file_count :usize = acj.iter().map(|&(_, ref v)| v.len()).sum();
+
+	println!("The target is {} files.", total_file_count);
+	let storage_base = env::current_dir().unwrap().join("crate-archives");
+	println!("Using directory {} to load the files from.",
+		storage_base.to_str().unwrap());
+
+	let needle = env::args().nth(1).expect("expected search term");
+	println!("Search term '{}'", needle);
+	let grepper = GrepBuilder::new(&needle).build().unwrap();
+
+	let (tx, rx) = sync_channel(10);
+
+	let thread_count = 8;
+	for v in 0..thread_count {
+		let tx = tx.clone();
+		let acj = acj.clone();
+		let grepper = grepper.clone();
+		let storage_base = storage_base.clone();
+		thread::spawn(move || {
+			run(tx, &acj, total_file_count, v, thread_count,
+				&grepper, &storage_base);
+		});
+	}
+	while let Ok((ctr, tc, s)) = rx.recv() {
+		println!("[{}/{}] {}", ctr, tc, s);
+	}
+}
